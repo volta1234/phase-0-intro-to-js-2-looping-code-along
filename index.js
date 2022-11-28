dae1057e49b6b546672edcf1349e180dc35ead05
CALVINCE <calvince.ogwel@gmail.com>
2022-11-28 18:37:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    let emptyArray= []
+    for(let i=0; i<names.length; i++){
+        const thankYouMessage= `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+     emptyArray.push(thankYouMessage)
+    }
+   return emptyArray
+
+}
+console.log(writeCards(["Joseph", "Calvince", "Otieno"], "Engagement"))
+
+
+//countdown function using while
+function countDown(){
+    let number=10;
+    while(number>=0){
+        console.log(number--)
+    }
+}
